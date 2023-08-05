5f6c8b954334da89c48f185094a94e8071ddf724
x0der <12592930+x0der00@users.noreply.github.com>
2023-08-05 17:13:17 +0330

    Update solve.ts
    Now it includes other digits too (0-9)

--- strings/tfcctf2023-MAYDAY/solve.ts
+++ strings/tfcctf2023-MAYDAY/solve.ts
@@ -1,5 +1,5 @@
 const msg: string = "Whiskey Hotel Four Tango Dash Alpha Romeo Three Dash Yankee Oscar Uniform Dash Sierra One November Kilo India November Golf Dash Four Bravo Zero Uniform Seven"
-const natoMessage = msg.replace(/Dash/g, "-").replace(/Zero/g, "0").replace(/One/g, "1").replace(/Three/g, "3").replace(/Four/g, "4").replace(/Seven/g, "7");
+const natoMessage = msg.replace(/Dash/g, "-").replace(/One/g, "1").replace(/Two/g, "2").replace(/Three/g, "3").replace(/Four/g, "4").replace(/Five/g, "5").replace(/Six/g, "6").replace(/Seven/g, "7").replace(/Eight/g, "8").replace(/Nine/g, "9").replace(/Zero/g, "0");
 
 const messageParts: string[] = natoMessage.split(" ");
 const decryptedMessage: string[] = []
