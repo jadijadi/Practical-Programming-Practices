dfba1031e751ee855d7cb52f36b3887316d42f26
fakher <101024125+fakher-alavi@users.noreply.github.com>
2023-08-09 02:09:02 +0330

    Add files via upload

---
+++ solve-fakher-alavi.py
@@ -0,0 +1,15 @@
+text = "T4 l16 _36 510 _27 s26 _11 320 414 {6 }39 C2 T0 m28 317 y35 d31 F1 m22 g19 d38 z34 423 l15 329 c12 ;37 19 h13 _30 F5 t7 C3 325 z33 _21 h8 n18 132 k24"
+string_list = text.split(' ')
+
+
+
+Ans = []
+for i in range(40):
+    Ans.append('*')
+
+for s in range(40):
+    Ans[int(string_list[s][1:])] = string_list[s][0]
+
+print('The decrypted message is: ',''.join(Ans))
+
+
