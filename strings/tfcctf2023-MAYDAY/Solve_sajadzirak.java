18551d939f75fd87d8edcadae6cf1c30eeaa1e3e
sajad <iriafmig29@gmail.com>
2023-08-09 23:07:35 +0330

    update java solution

--- strings/tfcctf2023-MAYDAY/Solve_sajadzirak.java
+++ strings/tfcctf2023-MAYDAY/Solve_sajadzirak.java
@@ -1,9 +1,10 @@
 import java.util.HashMap;
 
-public class Solve {
+public class Solve_sajadzirak {
     public static void main(String[] args) throws Exception {
         String call = "Whiskey Hotel Four Tango Dash Alpha Romeo Three Dash Yankee Oscar Uniform Dash Sierra One November Kilo India November Golf Dash Four Bravo Zero Uniform Seven";
 
+        // use a hashmap to keep special words meanings (numbers and Dash)
         HashMap<String, Character> specials = new HashMap<>();
         specials.put("Zero", '0');
         specials.put("One", '1');
@@ -17,12 +18,12 @@ public class Solve {
         specials.put("Nine", '9');
         specials.put("Dash", '-');
 
-        String[] words = call.split(" ");
+        String[] words = call.split(" "); // split the call string by space
         for(int i = 0; i < words.length; i++) {
-            if(specials.containsKey(words[i])) {
+            if(specials.containsKey(words[i])) { //check if our word does exist in the hashmap
                 System.out.print(specials.get(words[i]));
             }
-            else {
+            else { // if the word is not in the hashmap we simply print its first letter
                 System.out.print(words[i].charAt(0));
             }
         }
