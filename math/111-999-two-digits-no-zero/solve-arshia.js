a136f5d6ac8dd4f094275993fec081b7b7dea120
Arshia-Izadyar <arshiaa104@gmail.com>
2023-08-12 18:10:04 +0330

    add js solve

---
+++ math/111-999-two-digits-no-zero/solve-arshia.js
@@ -0,0 +1,27 @@
+function main(){
+    let count = 0;
+    for (let i=112; i< 1000; i++){
+        if (hasConditions(i)){
+            count++;
+        }
+    }
+    console.log(count);
+}
+
+
+function hasConditions(num) {
+    const digitSet = new Set(); // creating a set
+    for (i = 0; i < 3; i++){
+        let digit = num % 10;
+        num = Math.floor(num / 10); // finding numbers
+        digitSet.add(String(digit)) // adding str nums to a set
+    }
+    console.log(digitSet.size)
+    if (digitSet.size !== 2 || digitSet.has("0")){ // check if we have 0 in our set and our set length
+        return false;
+    }
+    return true
+}
+
+
+main() // calling the main so we can see the result
