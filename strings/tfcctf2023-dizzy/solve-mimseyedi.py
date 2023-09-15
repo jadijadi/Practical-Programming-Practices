423db42afdce58691fc6f11092d05d88ad01fc32
Mohammad <mim.seyedi@gmail.com>
2023-09-16 00:26:34 +0330

    mimseyedi solution

---
+++ strings/tfcctf2023-dizzy/solve-mimseyedi.py
@@ -0,0 +1,2 @@
+string = "T4 l16 _36 510 _27 s26 _11 320 414 {6 }39 C2 T0 m28 317 y35 d31 F1 m22 g19 d38 z34 423 l15 329 c12 ;37 19 h13 _30 F5 t7 C3 325 z33 _21 h8 n18 132 k24"
+print(''.join(map(lambda sorted_: sorted_[0], sorted(string.split(), key=lambda element: int(element[1:])))))
